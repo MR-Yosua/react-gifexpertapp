88750d010e9da77b0e35fda17eef04d68feeee32
Josue Lavado <j.lavadoortubia@gmail.com>
2020-07-28 16:57:21 -0400

    docs agregado

---
+++ docs/precache-manifest.bf61fd08a7b669cb706fe6c5d0f106e4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f213ac3758b0b6737c29cc0b08aff3c2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "398adfcb2ee048fb3805",
+    "url": "/static/css/main.259a1fa4.chunk.css"
+  },
+  {
+    "revision": "9d0467bdd98391920114",
+    "url": "/static/js/2.b04e268c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b04e268c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "398adfcb2ee048fb3805",
+    "url": "/static/js/main.3339e3b5.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
